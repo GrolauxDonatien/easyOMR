ab96d0c03e93a6cd5636afdbaea54a61194673e4
Donatien Grolaux <dgrolaux@grayswindle.com>
2023-06-29 17:10:27 +0200

    Updated version number

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const { app, BrowserWindow, dialog } = electron;
 const fs = require("fs");
 const fspath = require("path");
 const DEBUG = false;
-const VERSION = "1.0.60";
+const VERSION = "1.0.61";
 const QR_code = "lkhglqksdlfd9876098HMLKJSDFH-1";
 
 
